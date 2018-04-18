e81a1efb95a6616463ec1a8a24abcec7b52cc687
Hyun Min Choi <hyunmin.personal@gmail.com>
2018-04-18 18:17:51 +0900

    Delete target garbage and implement streaming ping

--- src/main/scala/com/devsisters/services/impl/PingServiceImpl.scala
+++ src/main/scala/com/devsisters/services/impl/PingServiceImpl.scala
@@ -2,10 +2,24 @@ package com.devsisters.services.impl
 
 import scala.concurrent.{ExecutionContext, Future}
 
+import io.grpc.stub.StreamObserver
+
 import com.devsisters.example.{PingReply, PingRequest}
 import com.devsisters.example.PingServiceGrpc.PingService
 
 class PingServiceImpl(implicit ec: ExecutionContext) extends PingService {
   def ping(request: PingRequest): Future[PingReply] =
     Future.successful(PingReply(request.payload))
+
+  def streamingPing(responseObserver: StreamObserver[PingReply]): StreamObserver[PingRequest] =
+    new StreamObserver[PingRequest] {
+      def onError(t: Throwable): Unit =
+        responseObserver.onError(t)
+
+      def onCompleted(): Unit =
+        responseObserver.onCompleted()
+
+      def onNext(value: PingRequest): Unit =
+        responseObserver.onNext(PingReply(value.payload))
+    }
 }
